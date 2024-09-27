2bdb4c834f5d34bfdb9d7b1496265f3d9d11a454
aadinew123 <brunnolr@hotmail.com>
2024-09-27 08:32:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ BOT_TOKEN = getenv("BOT_TOKEN", "8071359595:AAFxxV3iv--qby6oPrU0pDAL00DANKZ-nnA"
 OWNER_ID = int(getenv("OWNER_ID", "197064718"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://dustofappearance678:g1ETBVtzGV9ZcPJK@cluster0.bkxjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = int(getenv("LOG_GROUP", "-1002183779903"))
-FORCESUB = getenv("FORCESUB", "")
+FORCESUB = getenv("FORCESUB", "ilikeaadi")
 DEFAULT_SESSION = getenv("DEFAULT_SESSION", "") # this is jkust to help if you dont want to force your bot user to login or if they not interested
